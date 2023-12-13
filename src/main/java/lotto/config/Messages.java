f7d0a0b7e5f599e089fd42d1900e6a0e7630c11a
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-12-13 19:42:38 +0900

    fix(Messages) : 수익률 메시지 분할

--- src/main/java/lotto/config/Messages.java
+++ src/main/java/lotto/config/Messages.java
@@ -5,9 +5,10 @@ public enum Messages {
     INPUT_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
     NUMBER_OF_LOTTO_FORMAT("%d개를 구매했습니다.\n"),
-    RANK_FORMAT("%d개 일치 %s(%,d원) - %d개\n"),
-    BONUS_BALL("보너스 볼 일치 "),
-    PROFIT_RATIO_FORMAT("총 수익률은 %.1f%입니다.\n"),
+    RANK_FORMAT("%d개 일치%s (%,d원) - %d개\n"),
+    BONUS_BALL(", 보너스 볼 일치"),
+    PROFIT_RATIO_FORMAT("총 수익률은 %.1f"),
+    RECEIPT_TAIL("%입니다.\n"),
     RECEIPT_TITLE("당첨 통계\n---");
     private String message;
 
